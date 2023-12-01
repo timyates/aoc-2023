968901ac6c57be0c2660ee48b583079342de0496
Tim Yates <tim.yates@gmail.com>
2023-12-01 08:24:32 +0000

    day 1

---
+++ aoc/src/main/kotlin/com/bloidonia/aoc2023/day01/Main.kt
@@ -0,0 +1,68 @@
+package com.bloidonia.aoc2023.day01
+
+val example = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet""".lines()
+
+private val example2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen""".lines()
+
+private fun firstLast(s: String) = s.filter { it.isDigit() }.let {
+    "${it.first()}${it.last()}".toInt()
+}
+
+private val words = mapOf(
+    // Conjoined numbers first
+    "oneight" to 18,
+    "twone" to 21,
+    "threeight" to 38,
+    "fiveight" to 58,
+    "sevenine" to 79,
+    "eightwo" to 82,
+    "eighthree" to 83,
+    "nineight" to 98,
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
+
+private fun replacement(s: String): String {
+    var str = s
+
+    for (word in words.keys) {
+        str = str.replace(word, words[word].toString())
+    }
+    return str
+}
+
+fun main() {
+    val example = example.map { firstLast(it) }.sum()
+    println("example: $example")
+
+    val lines = object {}.javaClass.getResource("/day01.input")!!.readText().lines()
+
+    val part1 = lines.map { firstLast(it) }.sum()
+    println("$part1")
+
+    println("replacement: ${replacement("one")}")
+    val example2 = example2
+        .map { replacement(it) }
+        .map { firstLast(it) }
+        .sum()
+    println("example2: $example2")
+
+    val part2 = lines.map { replacement(it) }.map { firstLast(it) }.sum()
+    println("$part2")
+}
