f586a5207e703d5b71c4f9619283af5c34090126
Tim Yates <tim.yates@gmail.com>
2023-12-01 09:17:56 +0000

    Day 1 - alternative with regex

---
+++ aoc/src/main/kotlin/com/bloidonia/aoc2023/day01/Alternative.kt
@@ -0,0 +1,30 @@
+package com.bloidonia.aoc2023.day01
+
+val mapping = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
+
+private fun part1(s: String) = s.filter { it.isDigit() }.let {
+    "${it.first()}${it.last()}".toInt()
+}
+
+private fun part2(s: String) = Regex("(?=(\\d|one|two|three|four|five|six|seven|eight|nine))").let { regex ->
+    regex.findAll(s).map { it.groupValues[1] }.map { mapping[it] ?: it.toInt() }.let {
+        "${it.first()}${it.last()}".toInt()
+    }
+}
+
+fun main() {
+    val lines = object {}.javaClass.getResource("/day01.input")!!.readText().lines()
+
+    lines.map(::part1).sum().let(::println)
+    lines.map(::part2).sum().let(::println)
+}
