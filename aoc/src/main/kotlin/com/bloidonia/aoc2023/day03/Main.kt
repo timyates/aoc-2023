6693a29255d84c91ca45d1883fcae26b949208e6
Tim Yates <tim.yates@gmail.com>
2023-12-03 07:56:50 +0000

    day 03 cleanup

--- aoc/src/main/kotlin/com/bloidonia/aoc2023/day03/Main.kt
+++ aoc/src/main/kotlin/com/bloidonia/aoc2023/day03/Main.kt
@@ -4,6 +4,12 @@ import com.bloidonia.aoc2023.lines
 
 private data class Part(val x: IntRange, val y: Int, val value: String)
 private data class Symbol(val x: Int, val y: Int, val value: String)
+private data class Board(val parts: List<Part>, val symbols: List<Symbol>) {
+    constructor(input: List<String>) : this(
+        input.flatMapIndexed(::numberLocations),
+        input.flatMapIndexed(::symbolLocations)
+    )
+}
 
 private fun numberLocations(y: Int, line: String) = Regex("""\d+""")
     .findAll(line)
@@ -19,30 +25,18 @@ private fun partAdjacent(part: Part, symbols: List<Symbol>) = symbols.any { symb
     symbol.x in (part.x.first - 1..part.x.last + 1) && symbol.y in (part.y - 1)..(part.y + 1)
 }
 
-private fun part1(input: List<String>): Int {
-    val numbers = input.flatMapIndexed(::numberLocations)
-    val symbols = input.flatMapIndexed(::symbolLocations)
-
-    val filter = numbers.filter { n -> partAdjacent(n, symbols) }
-
-    return filter.sumOf { it.value.toInt() }
-}
-
 private fun numberAdjacent(symbol: Symbol, numbers: List<Part>) = numbers.filter { n ->
     (symbol.x - 1..symbol.x + 1).intersect(n.x).isNotEmpty()
             && n.y in (symbol.y - 1..symbol.y + 1)
 }
 
-private fun part2(input: List<String>): Long {
-    val numbers = input.flatMapIndexed(::numberLocations)
-    val symbols = input.flatMapIndexed(::symbolLocations)
+private fun part1(input: Board) = input.parts.filter { n -> partAdjacent(n, input.symbols) }
+    .sumOf { it.value.toInt() }
 
-    val gears = symbols.filter { it.value == "*" }
-
-    return gears.map { numberAdjacent(it, numbers) }
-        .filter { it.size == 2 }
-        .sumOf { it.first().value.toLong() * it.last().value.toLong() }
-}
+private fun part2(input: Board) = input.symbols.filter { it.value == "*" }
+    .map { numberAdjacent(it, input.parts) }
+    .filter { it.size == 2 }
+    .sumOf { it.first().value.toLong() * it.last().value.toLong() }
 
 private const val example = """467..114..
 ...*......
@@ -57,10 +51,12 @@ private const val example = """467..114..
 
 fun main() {
     val lines = lines("/day03.input")
+    val example = Board(example.lines())
+    val day03 = Board(lines)
 
-    part1(example.lines()).let(::println)
-    part1(lines).let(::println)
+    part1(example).let(::println)
+    part1(day03).let(::println)
 
-    part2(example.lines()).let(::println)
-    part2(lines).let(::println)
+    part2(example).let(::println)
+    part2(day03).let(::println)
 }
